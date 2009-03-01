fa6d94e0b70575de22bf28f23a91d109954855bf
phil.leveridge <phil.leveridge@8ac88bb8-03ee-11de-bd8f-71c2068a397f>
2009-03-01 15:09:06 +0000

    Skeleton support for battery charging

---
+++ battery.c
@@ -0,0 +1,25 @@
+//*****************************************************************************//
+// File Name	: 'battery.c'
+// Title		: Battery charging routines
+//
+//*****************************************************************************
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+#include "global.h"
+#include "rprintf.h"
+#include "Macro.h"
+#include "main.h"
+
+#include <util/delay.h>
+
+
+void batt_charge()
+{
+	// TBD
+}
+
