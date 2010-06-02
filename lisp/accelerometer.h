637ae13b71cd6472f5782ceb101cc0c4afafa146
phil.leveridge <phil.leveridge@8ac88bb8-03ee-11de-bd8f-71c2068a397f>
2010-06-02 17:24:55 +0000

    Insufficient memory for this too work - stack overflows

---
+++ lisp/accelerometer.h
@@ -0,0 +1,10 @@
+//==============================================================================
+// include file for accelerometer.c
+//==============================================================================
+
+extern void tilt_setup();
+extern void tilt_read();
+
+extern volatile int x_value;
+extern volatile int y_value;
+extern volatile int z_value;
