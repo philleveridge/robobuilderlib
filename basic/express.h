d5c09816538c2177e81115851a6cd66800984a8d
phil.leveridge@gmail.com <phil.leveridge@gmail.com@8ac88bb8-03ee-11de-bd8f-71c2068a397f>
2012-01-12 23:26:55 +0000

    further work on expressions, basic sums working

--- basic/express.h
+++ basic/express.h
@@ -11,7 +11,9 @@ extern unsigned char eval_expr(char **str, long *res);
 extern int			eval_list(char *p);
 extern long			math(long n1, long n2, char op);
 
-enum  TYPE {SYM, INT, BOOL, FUNC, FLOAT, STR, CELL, EMPTY};
+enum  TYPE		{SYM, INT, BOOL, FUNC, FLOAT, STR, CELL, EMPTY};
+enum  TOKTYP	{NUMI, NUMF, DELI, ALPHA, OPR};
+enum  MATHOP	{NA, PLUS, MINUS, DIVD, MULT, LAND, LOR};
 
 typedef struct object {
         int   type;
