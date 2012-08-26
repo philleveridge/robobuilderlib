9a7816f04574a1c5eca9d66d294e828d272d8989
phil.leveridge@gmail.com <phil.leveridge@gmail.com@8ac88bb8-03ee-11de-bd8f-71c2068a397f>
2012-08-26 10:57:47 +0000

    Added image filter creator

--- trunk/basic/BasicClient/BasicClient/Basic_frm.cs
+++ trunk/basic/BasicClient/BasicClient/Basic_frm.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.IO.Ports;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
 
 namespace RobobuilderLib
 {
@@ -561,6 +563,123 @@ namespace RobobuilderLib
                 output.Text = "";
                 //download_btn.Enabled = false;
                 fname.Text = "";
+            }
+        }
+
+        Bitmap n = null;
+        int minR, minG, minB;
+        int maxR, maxG, maxB;
+
+        void clear()
+        {
+            maxR = 0; maxG = 0; maxB = 0;
+            minR = 255; minG = 255; minB = 255;
+            show();
+        }
+
+        void show()
+        {
+            label4.Text = minB.ToString();
+            label5.Text = minG.ToString();
+            label6.Text = minR.ToString();
+
+            label7.Text = maxB.ToString();
+            label8.Text = maxG.ToString();
+            label9.Text = maxR.ToString();
+        }
+
+        void loadimage()
+        {
+            if (textBox1.Text.StartsWith("http://"))
+            {
+                Stream stream = File.OpenRead("temp.jpg");
+                n = new Bitmap(stream);
+                stream.Close();
+            }
+            else
+            {
+                n = new Bitmap(textBox1.Text);
+            }
+            pictureBox1.Image = n;
+            clear();
+        }
+
+        void filter()
+        {
+            for (int i = 0; i < n.Height; i++)
+            {
+                for (int j = 0; j < n.Width; j++)
+                {
+                    Color c = n.GetPixel(j, i);
+                    if (c.R >= minR && c.R <= maxR && c.B >= minB && c.B <= maxB && c.G >= minG && c.G <= maxG)
+                    {
+                        n.SetPixel(j, i, Color.White);
+                    }
+                    else
+                    {
+                        n.SetPixel(j, i, Color.Black);
+                    }
+                }
+
+                pictureBox1.Image = n;
+                pictureBox1.Update();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine("E=" + e.ToString() + sender.ToString());
+            MouseEventArgs m = (MouseEventArgs)e;
+            Console.Out.WriteLine("X=" + m.X + " Y=" + m.Y);
+            double fx = (double)pictureBox1.Image.Width * ((double)m.X / (double)pictureBox1.Size.Width);
+            double fy = (double)pictureBox1.Image.Height * ((double)m.Y / (double)pictureBox1.Size.Height);
+            Console.Out.WriteLine("X=" + (int)fx + " Y=" + (int)fy);
+            int x = (int)fx;
+            int y = (int)fy;
+            if (n != null)
+            {
+                if (minB > n.GetPixel(x, y).B) minB = n.GetPixel(x, y).B;
+                if (minG > n.GetPixel(x, y).G) minG = n.GetPixel(x, y).G;
+                if (minR > n.GetPixel(x, y).R) minR = n.GetPixel(x, y).R;
+
+                if (maxB < n.GetPixel(x, y).B) maxB = n.GetPixel(x, y).B;
+                if (maxG < n.GetPixel(x, y).G) maxG = n.GetPixel(x, y).G;
+                if (maxR < n.GetPixel(x, y).R) maxR = n.GetPixel(x, y).R;
+
+                show();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (n == null) return;
+            if (filtercb.Checked)
+                filter();
+            else
+            {
+                loadimage();
+            }
+        }
+
+        private void ldFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                loadimage();
+            }
+        }
+
+        private void ldWeb_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(textBox1.Text, "temp.jpg");
+                loadimage();
+            }
+            catch
+            {
             }
         }
 
