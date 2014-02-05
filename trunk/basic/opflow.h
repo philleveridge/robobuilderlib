c39d36dc7251816796d4f10d11b17f5604d897d0
phil.leveridge@gmail.com <phil.leveridge@gmail.com@8ac88bb8-03ee-11de-bd8f-71c2068a397f>
2014-02-05 12:52:39 +0000

    vj integration

---
+++ trunk/basic/opflow.h
@@ -0,0 +1,13 @@
+#ifndef OPFLOW_H
+#define OPFLOW_H
+
+/**********************************************************
+
+optic flow
+
+**********************************************************/
+
+extern tOBJ OpticFlow(tOBJ im1, tOBJ im2);
+
+#endif
+
