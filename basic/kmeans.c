c7cbd6c82808c5116c68562a8a63962a1426874a
phil.leveridge@gmail.com <phil.leveridge@gmail.com@8ac88bb8-03ee-11de-bd8f-71c2068a397f>
2014-08-01 22:39:03 +0000

    new image functions continues

--- basic/kmeans.c
+++ basic/kmeans.c
@@ -25,6 +25,8 @@ float dist(float a1,float b1,float a2,float b2)
 	return r;
 }
 
+//IMAGE KMEANS {200 0 200;200 0 200; 200 0 200} 2
+
 fMatrix *kmeans_mat(fMatrix *fm, int k)
 {
 	int i,j;
@@ -32,12 +34,10 @@ fMatrix *kmeans_mat(fMatrix *fm, int k)
 
 	if (fm->h < k) return NULL;  // must be more points than k !
 
-	fMatrix *rm = newmatrix(2,k);	// results
+	fMatrix *rm = newmatrix(3,k+fm->h);	// results
 
 	// initialise
 	int no_iterations=0;
-	float max_delta=0.001;
-	float delta=1.0;
 
 	for (i=0; i<k; i++)
 	{
@@ -45,21 +45,72 @@ fMatrix *kmeans_mat(fMatrix *fm, int k)
 		float y=fget2(fm,1,i);
 		fset2(rm,0,i,x); 
 		fset2(rm,1,i,y); 
+		fset2(rm,2,i,0); 
 	}
 
 	//loop
-	while (no_iterations<10 && delta >max_delta)
+
+	while (no_iterations<10)
 	{
+		float cx[k], cy[k];
 		no_iterations++;
-		for (i=k; i<fm->h; i++)
+		for (i=0; i<fm->h; i++)
 		{
-			float mx,my;
+			float mx,my,sd=10000.0;
+			int bk=0;
 			float x=fget2(fm,0,i);
 			float y=fget2(fm,1,i);
 			for (j=0; j<k; j++)
 			{
-				printf ("%d %d %f\n", i,j, dist(x,y, fget2(rm,0,j), fget2(rm,0,j)));
+				float d = dist(x,y, fget2(rm,0,j), fget2(rm,1,j));
+				if (d<sd) { sd=d; bk=j; }
+				//printf ("%d %d %f\n", i,j, d);
 			}
+			printf ("assigned to k=%d\n", bk);
+			fset2(rm,0,k+i,fget2(fm,0,i)); 
+			fset2(rm,1,k+i,fget2(fm,1,i)); 
+			fset2(rm,2,k+i,bk);
+			fmupdate(rm,2,bk,1); 
+		}
+
+		for (i=0; i<k; i++)
+		{
+			cx[i]=0.0;
+			cy[i]=0.0;
+		}
+
+		for (i=0; i<fm->h; i++)
+		{
+			// find new centre
+			float tx = fget2(rm,0,k+i);
+			float ty = fget2(rm,1,k+i);
+			int tk = (int)fget2(rm,2,k+i);
+
+			printf ("%d - %f %f\n", tk,tx,ty);
+
+			cx[tk] += tx;
+			cy[tk] += ty;
+		}
+
+		float dif = 0.0;
+		for (i=0; i<k; i++)
+		{
+			// find new centre
+			cx[i] /= fget2(rm,2,i); 
+			cy[i] /= fget2(rm,2,i); 
+
+			dif += pow((cx[i]-fget2(rm,0,i)),2) + pow((cy[i]-fget2(rm,1,i)),2);
+		}
+
+		printf ("dif= %f\n", dif);
+		if (dif <0.1) break;
+
+		for (i=0; i<k; i++)
+		{
+			// find new centre
+			fset2(rm,0,i,cx[i]); 
+			fset2(rm,1,i,cy[i]); 
+			fset2(rm,2,i,0); 
 		}
 	}
 
@@ -73,6 +124,7 @@ fMatrix *kmeans_img(oImage *img, int k, int th)
 	int cnt=0;
 
 	// setup nm
+	if (img==NULL) return NULL;
 
 	for (i=0; i<img->w*img->h; i++) 
 		if (img->data[i]>th) cnt++;
